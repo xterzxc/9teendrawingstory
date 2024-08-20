ff86a279bca44dbb2d10cad34d8937af93c0f53e
xterzxc <valera.bondarev2005@gmail.com>
2024-08-20 20:09:59 +0200

    random hash for link generation maded

---
+++ ntds/draw/utils.py
@@ -0,0 +1,15 @@
+import string
+import random
+
+def generate_link(num, length=8):
+    alphabet = string.digits + string.ascii_letters
+
+    base = len(alphabet)
+    encoded = []
+    while num:
+        num, rem = divmod(num, base)
+        encoded.append(alphabet[rem])
+    while len(encoded) < length:
+        
+        encoded.append(random.choice(alphabet))
+    return ''.join(reversed(encoded))
