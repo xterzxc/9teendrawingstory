c68da4ff1cced52209c85fc48aeca911dfd39164
9teen <hqmans333@gmail.com>
2024-08-22 22:56:58 +0500

    adding clear button function and working with key combinations

--- ntds/static/js/draw.js
+++ ntds/static/js/draw.js
@@ -2,18 +2,31 @@ function $(el) {
     return document.getElementById(el.replace(/#/, ''));
 }
 
+
 var canvas = $('#canvas');
 canvas.width = document.body.clientWidth;
 canvas.height = document.body.clientHeight;
 var context = canvas.getContext('2d');
 const undoBtn = document.getElementById('undoBtn');
 const redoBtn = document.getElementById('redoBtn');
+const saveBtn = document.getElementById('saveBtn');
+const clearBtn = document.getElementById('clearBtn');
+
+
 
 var colorPicker = $('#colorPicker');
 var lineWidth = $('#lineWidth');
 
 var undoStack = [];
 var redoStack = [];
+
+function fillBackground(color) {
+    context.fillStyle = color;
+    context.fillRect(0, 0, canvas.width, canvas.height);
+}
+
+// Заполняем холст фоном при загрузке
+fillBackground('#ffffff'); // Белый фон
 
 function getCanvasCoordinates(e) {
     var rect = canvas.getBoundingClientRect();
@@ -120,3 +133,29 @@ function redo() {
 
 undoBtn.addEventListener('click', undo);
 redoBtn.addEventListener('click', redo);
+
+saveBtn.addEventListener('click', function() {
+    const dataURL = canvas.toDataURL('image/png');
+    
+    const link = document.createElement('a');
+    link.href = dataURL;
+    link.download = 'canvas-image.png'; 
+    link.click();
+});
+
+document.addEventListener('keydown', function(e) {
+    if (e.ctrlKey && e.key === 'z' || e.code === 'KeyZ') {
+        e.preventDefault(); 
+        undo();
+    } else if (e.ctrlKey && e.key === 'y' || e.code === 'KeyY') {
+        e.preventDefault(); 
+        redo();
+    }
+});
+
+function clear(){
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    undoStack.splice(0, undoStack.length);
+    redoStack.splice(0, redoStack.length);
+};  
+clearBtn.addEventListener('click', clear);
