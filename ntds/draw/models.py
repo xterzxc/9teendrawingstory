a6b3befe7eaeb42766c68a0e22bae48d247e4d64
xterzxc <valera.bondarev2005@gmail.com>
2024-08-22 17:48:04 +0200

    maded controls on drawing page

--- ntds/draw/models.py
+++ ntds/draw/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Drawing(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=200, null=True)
+    likes = models.IntegerField(default=0)
+    # comments = manytomanyfield
+    created_at = models.DateTimeField(auto_now_add=True)
+    imglink = models.URLField()
+
+    def __str__(self):
+        return self.title
--- ntds/static/js/draw.js
+++ ntds/static/js/draw.js
@@ -1,23 +1,46 @@
 function $(el) {
     return document.getElementById(el.replace(/#/, ''));
-};
+}
 
 var canvas = $('#canvas');
 canvas.width = document.body.clientWidth;
 canvas.height = document.body.clientHeight;
 var context = canvas.getContext('2d');
+const undoBtn = document.getElementById('undoBtn');
+const redoBtn = document.getElementById('redoBtn');
+
+var colorPicker = $('#colorPicker');
+var lineWidth = $('#lineWidth');
+
+var undoStack = [];
+var redoStack = [];
+
+function getCanvasCoordinates(e) {
+    var rect = canvas.getBoundingClientRect();
+    return {
+        x: (e.clientX || e.targetTouches[0].pageX) - rect.left,
+        y: (e.clientY || e.targetTouches[0].pageY) - rect.top
+    };
+}
+
+function saveState() {
+    undoStack.push(canvas.toDataURL());
+    redoStack = [];
+}
 
 var start = function(coors) {
-    context.beginPath();
-    context.moveTo(coors.x, coors.y);
-    this.isDrawing = true;
+    if (!this.isDrawing) {
+        context.beginPath();
+        context.moveTo(coors.x, coors.y);
+        this.isDrawing = true;
+    }
 };
 
 var move = function(coors) {
     if (this.isDrawing) {
-        context.strokeStyle = "#fff";
+        context.strokeStyle = colorPicker.value;
         context.lineJoin = "round";
-        context.lineWidth = 3;
+        context.lineWidth = lineWidth.value;
         context.lineTo(coors.x, coors.y);
         context.stroke();
     }
@@ -25,8 +48,9 @@ var move = function(coors) {
 
 var stop = function(coors) {
     if (this.isDrawing) {
-        this.touchmove(coors);
+        move(coors);
         this.isDrawing = false;
+        saveState();
     }
 };
 
@@ -41,10 +65,7 @@ var drawer = {
 };
 
 var draw = function(e) {
-    var coors = {
-        x: e.clientX || e.targetTouches[0].pageX,
-        y: e.clientY || e.targetTouches[0].pageY
-    };
+    var coors = getCanvasCoordinates(e);
     drawer[e.type](coors);
 };
 
@@ -55,7 +76,6 @@ canvas.addEventListener('touchstart', draw, false);
 canvas.addEventListener('touchmove', draw, false);
 canvas.addEventListener('touchend', draw, false);
 
-// prevent elastic scrolling
 document.body.addEventListener('touchmove', function(e) {
     e.preventDefault();
 }, false);
@@ -63,4 +83,40 @@ document.body.addEventListener('touchmove', function(e) {
 window.onresize = function(e) {
     canvas.width = document.body.clientWidth;
     canvas.height = document.body.clientHeight;
+    if (undoStack.length > 0) {
+        restoreState(undoStack, false);
+    }
 };
+
+function restoreState(stack, pop = true) {
+    if (stack.length === 0) return;
+    const dataURL = stack[stack.length - 1];
+    if (pop) stack.pop()
+    const img = new Image();
+    img.src = dataURL;
+    img.onload = () => {
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        context.drawImage(img, 0, 0);
+    };
+}
+
+function undo() {
+    if (undoStack.length > 1) {
+        redoStack.push(undoStack.pop());
+        restoreState(undoStack, false);
+    } else if (undoStack.length === 1) {
+        redoStack.push(undoStack.pop());
+        context.clearRect(0, 0, canvas.width, canvas.height);
+    }
+}
+
+function redo() {
+    if (redoStack.length > 0) {
+        const lastState = redoStack.pop();
+        undoStack.push(lastState);
+        restoreState(undoStack, false);
+    }
+}
+
+undoBtn.addEventListener('click', undo);
+redoBtn.addEventListener('click', redo);
