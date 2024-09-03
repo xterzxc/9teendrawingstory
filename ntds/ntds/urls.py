98b9f9fc9f7c81f1dd95d388b62c1994839e27c9
xterzxc <valera.bondarev2005@gmail.com>
2024-09-03 21:17:06 +0200

    registration and login complete

--- ntds/ntds/urls.py
+++ ntds/ntds/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', IndexView.as_view(), name='index'),
     path('draw/', include('draw.urls')),
+    path('usr/', include('users.urls')),
 ]
---
+++ ntds/users/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path, include
+from .views import *
+
+urlpatterns = [
+    path("accounts/", include("django.contrib.auth.urls")),
+    path('signup/', SignUpView.as_view(), name='signup'),
+]
