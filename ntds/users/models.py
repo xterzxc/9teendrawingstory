c4ef256f9e4717c92cc992ff047838e4d3f45d54
9teen <hqmans333@gmail.com>
2024-09-05 22:06:20 +0500

    Redesign of the main page and adding display during registration

--- ntds/users/models.py
+++ ntds/users/models.py
@@ -1,3 +1,10 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    bio = models.TextField(blank=True)
+    birth_date = models.DateField(null=True, blank=True)
+    img
+
+    def str(self):
+        return self.username
--- ntds/users/urls.py
+++ ntds/users/urls.py
@@ -4,4 +4,4 @@ from .views import *
 urlpatterns = [
     path("accounts/", include("django.contrib.auth.urls")),
     path('signup/', SignUpView.as_view(), name='signup'),
-]
+] 
