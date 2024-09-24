5bcf7e09ec8d27187edda1727901b2dc19a8cb68
Valerii Bondariev <valera.bondarev2005@gmail.com>
2024-09-24 23:33:10 +0200

    added command to generate random users + drawings

--- ntds/ntds/views.py
+++ ntds/ntds/views.py
@@ -6,11 +6,6 @@ from .utils import CF_ACCESS
 class IndexView(TemplateView):
     template_name = "base/index.html"
     def get_context_data(self, **kwargs):
-        cf_access = CF_ACCESS()
-        cf_delete, status_code = cf_access.delete('canvas-image(5).png')
-        print(status_code)
-        if status_code == 204:
-            print('DELETED')
         context = super().get_context_data(**kwargs)
         context['images'] = Drawing.objects.all()
         return context
